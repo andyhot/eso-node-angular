6bd3fb25c1ef425aec92e2e780e41a1b4317dcd0
Andreas Andreou <andreoua@gmail.com>
2013-06-11 13:50:17 +0300

    setup grunt build + angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('esoNodeApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
